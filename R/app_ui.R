ebea31a3f5c279362f74ef10a6e8335958799f1a
Chase Clark <cclark42@uic.edu>
2019-03-06 19:47:12 -0600

    Update app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,9 +3,6 @@ app_ui <- function() {
   tagList(
     # Leave this function for adding external resources
     # from inst/app/www
-    golem_add_external_resources(),
-    golem::js(),
-    golem::favicon(),
     # List the first level UI elements here 
     fluidPage(
       h1("demoApp")
